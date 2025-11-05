d4daf30928ec56d10664cdfdd46838856c8794f2
abinash7854053195-design <abinash7854053195@gmail.com>
2025-11-06 02:47:30 +0530

    Delete .vscode directory

--- .vscode/abi5.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main() {
-    //check a given number is positive or negative
-    int num;
-    printf("enter a number :");
-    scanf("%d",&num);
-    num >= 0 ? printf("the number is positive:") : printf("the number is negative:");
-    return 0;
-}
