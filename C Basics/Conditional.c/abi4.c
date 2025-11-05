1fbc402689d7792fdfdb4a0e83bf6c3cd12aee41
abinash7854053195-design <abinash7854053195@gmail.com>
2025-11-06 02:47:13 +0530

    Delete C Basics directory

--- C Basics/Conditional.c/abi4.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main() {
-    //even or odd by using ternary operater
-    int num;
-    printf("enter a number :");
-    scanf("%d",&num);
-    num % 2 == 0?printf("the number is even:"):printf("the number is odd:");
-    return 0;
-}
